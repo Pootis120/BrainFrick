7b4a5d289157c7eea2afa9b645b598a4e272c3e5
unknown <danya.mokhov@gmail.com>
2022-05-28 22:12:42 +0300

    refactor: Changed to reading file contents from string not stream

--- BrainFrick/src/Main.cpp
+++ BrainFrick/src/Main.cpp
@@ -7,27 +7,16 @@ int main(int argc, char** argv)
 {
 	bool quit = false;
 	std::string fileName;
-	std::ifstream file;
 	while (!quit) 
 	{
 		std::cout << "Input file location: ";
 		//std::cin >> fileName;
 		fileName = "C:\\Dev\\Cpp\\Test.txt";
-		file.open(fileName);
-		if (file.is_open()) 
-		{
-			Interpreter prog;
-			std::cout << "File opened" << std::endl;
-			char ch;
-			while (file >> std::noskipws >> ch) {
-				std::cout << ch;
-			}
-		}
-		else 
-		{
-			std::cout << "Could not open file" << std::endl;
-		}
-		file.close();
+		std::ifstream file(fileName);
+		std::string contents((std::istreambuf_iterator<char>(file)),
+			std::istreambuf_iterator<char>());
+		std::cout << contents << std::endl;
+		std::cin.get();
 	}
 	return 0;
 }
