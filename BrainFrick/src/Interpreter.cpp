05a88bd10970a40684225678d7a87a7ed5418966
unknown <danya.mokhov@gmail.com>
2022-05-28 13:22:40 +0300

    feat: Added basic keyword functions like add and move

--- BrainFrick/src/Interpreter.cpp
+++ BrainFrick/src/Interpreter.cpp
@@ -2,3 +2,23 @@
 
 Interpreter::Interpreter() {}
 Interpreter::~Interpreter() {}
+
+void Interpreter::MovePcLeft() 
+{
+	Pc--;
+}
+
+void Interpreter::MovePcRight()
+{
+	Pc++;
+}
+
+void Interpreter::Add()
+{
+	mem[Pc]++;
+}
+
+void Interpreter::Sub()
+{
+	mem[Pc]--;
+}
--- BrainFrick/src/Interpreter.h
+++ BrainFrick/src/Interpreter.h
@@ -2,8 +2,19 @@
 class Interpreter
 {
 private:
+	unsigned short Pc = 1000;
+	int mem[2000];
 public:
 	Interpreter();
 	~Interpreter();
+
+	void MovePcLeft();
+	void MovePcRight();
+	void Add();
+	void Sub();
+	void StartLoop();
+	void EndLoop();
+	void Print();
+	void GetChar();
 };
 
