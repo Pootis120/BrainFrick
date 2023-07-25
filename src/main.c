9073d6d91e9b2ffa60e80e4d15b2220a6cb6cff5
Pootis120 <danya.mokhov@gmail.com>
2023-07-25 22:50:30 +0300

    feat: Added ability to read file and added gitignore

---
+++ src/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void ASSERT(bool initialized, char* messageOnFail) 
+{
+  if (!initialized) 
+  {
+    printf("%s", messageOnFail);
+  }
+}
+
+int main() {
+  FILE* inputFile;
+  char* outtext;
+  long numbytes;
+
+  inputFile = fopen("source.txt", "r");
+  ASSERT(inputFile, "Error: Could not open file");
+  fseek(inputFile, 0L, SEEK_END);
+  numbytes = ftell(inputFile);
+  fseek(inputFile, 0L, SEEK_SET);
+
+  outtext = (char*)calloc(numbytes, sizeof(char));
+  ASSERT(outtext, "Could not initialize text");
+  fread(outtext, sizeof(char), numbytes, inputFile);
+  fclose(inputFile);
+  printf("%s\n", outtext);
+  return 0;
+} 
