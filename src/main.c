4ae1b4216e00e363e83fc34f066fc5d082dd5e66
Pootis120 <danya.mokhov@gmail.com>
2023-07-26 12:26:26 +0300

    feat: Added memory as a struct and completed basic interpreter as a function (without loop brackets)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 void ASSERT(bool initialized, char* messageOnFail) 
 {
@@ -30,10 +31,52 @@ char* ReadFile(char* filePath)
   return outtext;
 }
 
+//commit to github: feat: made struct and interpreter
+
+struct MemoryVars 
+{
+  int Pc;
+  int* memory;
+};
+
+void Interpret(char* sourceCode) 
+{
+  struct MemoryVars mem;
+  mem.memory = (int*)calloc(100 * sizeof(int), sizeof(int));
+  mem.Pc = 0;
+  for (int i = 0, max = strlen(sourceCode); i < max; i++) 
+  {
+    switch (sourceCode[i]) 
+    {
+      case '>' :
+        mem.Pc++;
+        break;
+      case '<' :
+        mem.Pc--;
+        break;
+      case '+' :
+        mem.memory[mem.Pc]++;
+        break;
+      case '-' :
+        mem.memory[mem.Pc]--;
+        break;
+      case '.' :
+        putchar(mem.memory[mem.Pc]);
+        break;
+      case ',' :
+        mem.memory[mem.Pc] = getchar();
+        break;
+      default:
+        break;
+    }
+  }
+}
+
 int main() {
   char* text = ReadFile("source.txt");
+  Interpret(text);
   
-  printf("%s\n", text);
+  printf("\n");
   free(text);
   return 0;
 } 
